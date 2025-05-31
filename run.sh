7975b7b156b2d609bd8e185edc572ee9554b353b
PranavGaur7 <raghavgau7@gmail.com>
2025-05-31 18:29:39 +0000

    zset added

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # g++ server.cpp -o server
-g++ server.cpp hashtable.cpp avl.cpp test_avl.cpp -o server
+g++ server.cpp hashtable.cpp avl.cpp zset.cpp -o server
 g++ client.cpp -o client
